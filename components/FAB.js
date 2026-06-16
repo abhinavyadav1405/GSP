1695a3cefd203ebe8cfb3d0c0e819e24b7a1b9a2
abhinavyadav1405 <abhinavyadav1405007@gmail.com>
2026-06-16 20:24:22 +0530

    Create FAB.js

---
+++ components/FAB.js
@@ -0,0 +1,11 @@
+import FAB from '../components/FAB';
+import CategoryGrid from '../components/CategoryGrid';
+
+export default function Home() {
+  return (
+    <div>
+      <CategoryGrid />
+      <FAB />
+    </div>
+  );
+}
